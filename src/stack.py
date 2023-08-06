2c5f956f2edc177d00e21f9ab7aef42f02e3cc2b
Victor Sadykov <svn1409@gmail.com>
2023-08-07 02:24:29 +0800

    update class Node and Stack in stack.py update test update main.py (для красивого отображения)

--- src/stack.py
+++ src/stack.py
@@ -1,24 +1,34 @@
+
 class Node:
     """Класс для узла стека"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
         """
         Конструктор класса Node
 
         :param data: данные, которые будут храниться в узле
+        next_node - ссылка на следующий узел стека
         """
         self.data = data
         self.next_node = next_node
 
     def __str__(self):
-        return f'{self.data}, {self.next_node}'
+        return f'{self.data} [{self.next_node}]'
 
     def __repr__(self):
-        return f'Node({self.data}, {self.next_node})'
+        return f'Node({self.data} [{self.next_node}])'
 
 
 class Stack:
     """Класс для стека"""
+
+    #элементы стека
+    all = []
+
+    # возвращаем последний элемент списка
+    def top(self):
+        return Stack.all[-1]
+
 
     def __init__(self):
         """Конструктор класса Stack"""
@@ -30,7 +40,10 @@ class Stack:
 
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        if Stack.all == []:
+            Stack.all.append(Node(data))
+        else:
+            Stack.all.append(Node(data, Stack.all[-1]))
 
     def pop(self):
         """
@@ -38,4 +51,19 @@ class Stack:
 
         :return: данные удаленного элемента
         """
-        pass
+        return Stack.all.pop(-1)
+
+    def print_stack(self):
+        """
+        Метод формирования строки для печати стека
+
+        :return: сттрока с данными стека
+        """
+        if Stack.all == []:
+            result = 'пустой'
+        for item in Stack.all:
+            if item.next_node == None:
+                result = f'{item.data}'
+            else:
+                result = f'{item.data} -> {result}'
+        return result
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,3 +1,24 @@
 import unittest
+from src.stack import Node, Stack
 
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+
+def test_Node():
+    n1 = Node(5)
+    n2 = Node('a', n1)
+    assert n1 == '5 [None]'
+    assert n2 == 'a [5 [None]]'
+    assert n1.__str__() == '5 [None]'
+    assert n1.__repr__() == 'Node(5, None)'
+
+
+def test_Stack():
+    stack = Stack()
+    assert stack.print_stack() == 'пустой'
+    stack.push('data1')
+    assert stack.print_stack() == 'data1'
+    stack.push('data2')
+    assert stack.print_stack() == 'data2 -> data1'
+    assert stack.top().data == 'data2'
+    assert stack.pop == 'data2'
+    assert stack.print_stack() == 'data1'
