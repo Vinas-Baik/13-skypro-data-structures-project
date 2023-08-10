0ea73e9df4897bd6867dd1b16f44d803fbf1e217
Victor Sadykov <svn1409@gmail.com>
2023-08-10 20:02:25 +0800

    Home Work 3 update queue.py

--- src/queue.py
+++ src/queue.py
@@ -1,21 +1,31 @@
 class Node:
     """Класс для узла очереди"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
         """
         Конструктор класса Node
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.__data = data
+        self.__next_node = next_node
+
+    def __str__(self):
+        return f'{self.__data} [{self.__next_node}]'
+
+    def __repr__(self):
+        return f'Node({self.__data} [{self.__next_node}])'
 
 
 class Queue:
     """Класс для очереди"""
 
+    # элементы очереди
+    all = []
+
     def __init__(self):
         """Конструктор класса Queue"""
-        pass
+        Queue.all = []
 
     def enqueue(self, data):
         """
@@ -23,16 +33,21 @@ class Queue:
 
         :param data: данные, которые будут добавлены в очередь
         """
-        pass
+        if Queue.all == []:
+            Queue.all.append(Node(data))
+        else:
+            Queue.all.append(Node(data, Queue.all[-1]))
 
-    def dequeue(self):
+
+    def dequeue(self) -> Node:
         """
         Метод для удаления элемента из очереди. Возвращает данные удаленного элемента
 
         :return: данные удаленного элемента
         """
-        pass
+        if Queue.all != []:
+            return Queue.all.pop(0)
 
     def __str__(self):
         """Магический метод для строкового представления объекта"""
-        pass
+        return '\n'.join(temp_q.data for temp_q in Queue.all)
