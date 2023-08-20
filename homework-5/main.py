f8893a8488f5dd0dc7576e5f593ed8fbb8063149
Victor Sadykov <svn1409@gmail.com>
2023-08-21 00:20:17 +0800

    Home Work 5 update main.py - красота update linked_list.py

--- homework-5/main.py
+++ homework-5/main.py
@@ -1,15 +1,35 @@
 from src.linked_list import LinkedList
 
+ll = LinkedList()
+
+
+def add_list(data, head: bool):
+    print(f"\tДобавляем \033[35m{data}\033[39m в {'начало списка' if head else 'конец списка'}")
+
+    if head:
+        ll.insert_beginning(data)
+    else:
+        ll.insert_at_end(data)
+
+    print(f'\t\033[32mСписок\033[39m: {ll}')
+    print(f'\tНачало списка: \033[33m{ll.head.data}\033[39m, '
+          f'Конец списка: \033[33m{ll.tail.data}\033[39m\n')
+
+
 if __name__ == '__main__':
     # Создаем пустой односвязный список
-    ll = LinkedList()
+    # ll = LinkedList()
+    print(f'\033[32mСписок\033[39m: {ll}')
 
     # Добавляем данные
-    ll.insert_beginning({'id': 1})
-    ll.insert_at_end({'id': 2})
-    ll.insert_at_end({'id': 3})
-    ll.insert_beginning({'id': 0})
+    print('\nДобавляем данные в список:\n')
+    for i in [(1, True),
+              (2, False),
+              (3, False),
+              (0, True)]:
+        add_list({'id': i[0]}, i[1])
+
 
     # Печатаем данные
-    print(ll)
+    print(f'Список: {ll}')
     assert str(ll) == "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None"
--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,51 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+
+    def __init__(self, data, next_node: 'Node'=None):
+        """
+        Конструктор класса Node
+
+        :param data: данные, которые будут храниться в узле
+        next_node - ссылка на следующий узел
+        """
+        self.data = data
+        self.next_node = next_node
+
+    def __str__(self):
+        return f'{self.data} [{self.next_node}]'
+
+    def __repr__(self):
+        return f'Node({self.data} [{self.next_node}])'
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        if self.head == None:
+            self.head = Node(data)
+            self.tail = self.head
+        else:
+            temp_node = Node(data, self.head)
+            self.head = temp_node
+
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        if self.head == None:
+            self.head = Node(data)
+            self.tail = self.head
+        else:
+            temp_node = self.head
+            while temp_node.next_node !=None:
+                temp_node = temp_node.next_node
+            temp_node.next_node = Node(data)
+            self.tail = temp_node.next_node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -27,3 +60,5 @@ class LinkedList:
 
         ll_string += ' None'
         return ll_string
+
+
